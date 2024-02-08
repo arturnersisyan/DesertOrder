26de35ca717934027df89689eac3712d8c0ea5b1
arturnersisyan <artnersisyan2000@gmail.com>
2024-02-08 13:05:51 +0400

    Cart dialog opening and closing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Header from "./components/Header";
 import Deserts from "./components/Deserts";
+import Cart from "./components/Cart";
 import { CartContextProvider } from "./store/CarContext"; 
+import { UserProgressContextProvider } from "./store/UserProgressContext";
 
 function App() {
   return (
-    <CartContextProvider>
-      <Header />
-      <Deserts />
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Deserts />
+        <Cart />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
